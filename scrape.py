2d694c4044e9bd3e01ee70faa9ced7d345109d3c
mhkuu <M.H.vanderKlis@uu.nl>
2016-03-24 12:30:21 +0100

    Small fixes for the scraper

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import re
 import os
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 BASE_URL = 'http://www.dbnl.org/tekst/mand001schi01_01/'
 PAGE_NUMBER = re.compile(r"""
@@ -29,13 +29,20 @@ def scrape_page(page, folder, current_file=None):
                     continue
 
             if current_file:
-                with codecs.open(os.path.join(folder, current_file + '.txt'), 'ab') as out_file:
-                    for child in ch.children:
-                        if child.name in ['h1', 'h2', 'h3', 'h4', 'p']:
-                            write_line(child, out_file)
-                        # Special handlers for poems
-                        if child.name == 'div' and child['class'][0] in ['poem', 'poem-small-margins']:
-                            write_poem(child, out_file)
+                lines = []
+                # Retrieve the text elements from the page
+                for child in ch.children:
+                    if child.name in ['h1', 'h2', 'h3', 'h4', 'p']:
+                        lines.append(child)
+                    # Special handlers for poems
+                    if child.name == 'div' and child['class'][0] in ['poem', 'poem-small-margins']:
+                        lines.extend(get_poem(child))
+
+                # Write the lines to an output file
+                if lines:
+                    with codecs.open(os.path.join(folder, current_file + '.txt'), 'ab') as out_file:
+                        for line in lines:
+                            write_line(line, out_file)
 
     return current_file
 
@@ -49,20 +56,23 @@ def strip_encode(line):
 
 def write_line(line, out_file):
     """
-    Writes a line to the given out_file, and ends it with an enter.
+    Writes a line to the given out_file, and ends it with a new line.
     """
-    out_file.write(strip_encode(line.text))
-    out_file.write('\n')
+    line = strip_encode(line.text)
+    if line:
+        out_file.write(line)
+        out_file.write('\n')
 
 
-def write_poem(poem, out_file):
+def get_poem(poem):
     """
-    Writes a complete poem to the given out_file.
+    Retrieves all the lines of a complete poem to the given out_file.
     """
+    lines = []
     for line in poem.children:
         if line.name == 'div' and line['class'][0] in ['poem-head', 'line']:
-            out_file.write(strip_encode(line.text))
-        out_file.write('\n')
+            lines.append(line)
+    return lines
 
 if __name__ == "__main__":
     # Retrieve the table of contents
@@ -76,24 +86,24 @@ if __name__ == "__main__":
     current_folder = None
     previous_file = None
     for p in toc.parent.next_siblings:
-        if p.find('a') != -1:
-            a = p.find('a')
-            url = BASE_URL + a['href']
+        if type(p) == element.Tag:
+            for a in p.find_all('a'):
+                url = BASE_URL + a['href']
 
-            if 'head2' in a['class']:
-                chapter_nr += 1
-                section_nr = 0
-                current_folder = os.path.join(OUT_FOLDER, 'h{}'.format(chapter_nr))
-                previous_file = None
-            elif 'head3' in a['class']:
-                section_nr += 1
-                current_folder = os.path.join(OUT_FOLDER, 'h{}'.format(chapter_nr), 'c{}'.format(section_nr))
+                if 'head2' in a['class']:
+                    chapter_nr += 1
+                    section_nr = 0
+                    current_folder = os.path.join(OUT_FOLDER, 'h{0:02d}'.format(chapter_nr))
+                    previous_file = None
+                elif 'head3' in a['class']:
+                    section_nr += 1
+                    current_folder = os.path.join(OUT_FOLDER, 'h{0:02d}'.format(chapter_nr), 'c{0:02d}'.format(section_nr))
 
-            if not os.path.exists(current_folder):
-                os.makedirs(current_folder)
+                if not os.path.exists(current_folder):
+                    os.makedirs(current_folder)
 
-            print 'Now processing {}'.format(url)
-            previous_file = scrape_page(requests.get(url).content, current_folder, previous_file)
-            processed += 1
-            if processed == 5:  # prevent scraping the whole database on the first try :-)
-                break
+                print 'Now processing {}'.format(url)
+                previous_file = scrape_page(requests.get(url).content, current_folder, previous_file)
+                processed += 1
+        if processed == 5:  # prevent scraping the whole database on the first try :-)
+            break
